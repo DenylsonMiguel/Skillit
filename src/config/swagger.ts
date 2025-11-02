7c08a1d6ceb39b157aa4831c609f575956f229d8
DenylsonMiguel <denylsongpereira@gmail.com>
2025-11-02 12:28:25 -0300

    Swagger Adicionado

---
+++ src/config/swagger.ts
@@ -0,0 +1,35 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import type { Express } from "express";
+import "dotenv/config";
+
+if (!process.env.HOST)
+    throw new Error("Host not defined");
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Skillit API",
+      version: "1.0.0",
+      description: "Skillit API documentation",
+    },
+    servers: [
+      {
+        url: process.env.HOST,
+      },
+    ],
+  },
+  apis: ["src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export function setupSwagger(app: Express): void {
+    try {
+        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+        console.log("Swagger started \n");
+    } catch (err) {
+        throw new Error("Error on start Swagger: " + err);
+    }
+}
